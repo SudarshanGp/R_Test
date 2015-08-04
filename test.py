a83fdcd448d396a3d3ec4a480b4cf44d6c74a6f0
SudarshanGp <Sudarshan@Sudarshans-MacBook-Air.local>
2015-08-03 20:21:29 -0500

    TEst

--- test.py
+++ test.py
@@ -24,7 +24,29 @@ output['children'].append(
 	'name' : 'Gender',
 	'children' : gender['children']
 })
-json.dump(output, jsonfile)
-print "success"
-jsonfile.close()
 csvfile.close()
+print "success 1"
+csvfile = open('su15_2.csv', 'r')
+instate = {'children' : []}
+a = {'test' : []}
+for row in csv.DictReader(csvfile):
+	a['test'].append({'name' : 'Instate', 'size' : row['Illinois']
+		})
+	a['test'].append({'name' : 'Outstate', 'size' : row['Non-Illinois']
+		})
+	instate['children'].append(
+		{
+		'name' : row['Courses'],
+		'children' : a['test']
+		})
+	a = {'test' : []}
+
+output['children'].append(
+{
+	'name' : 'Instate',
+	'children' : instate['children']
+})
+
+
+json.dump(output, jsonfile)
+jsonfile.close()
